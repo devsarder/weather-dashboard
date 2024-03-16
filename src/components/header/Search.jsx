325cd798bb2f2fa6c236b7bfe1ce3c9dc6a5dd0f
devsarder <devssarder@gmail.com>
2024-03-16 14:38:24 +0100

    Refactor search component to utilize custom 'useDebounce' hook

--- src/components/header/Search.jsx
+++ src/components/header/Search.jsx
@@ -2,22 +2,32 @@ import { useContext, useState } from "react";
 import SearchSvg from "../../assets/search.svg";
 import { LocationContext } from "../../context";
 import { getLocationByName } from "../../data/location-data";
+import { useDebounce } from "../hooks";
 export default function Search() {
   const [searchTerm, setSearchTerm] = useState("");
   const { setSelectedLocation } = useContext(LocationContext);
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(searchTerm);
-    const searchedLocation = getLocationByName(searchTerm.toLowerCase());
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   console.log(searchTerm);
+  //   const searchedLocation = getLocationByName(searchTerm.toLowerCase());
+  //   console.log(searchedLocation);
+  //   setSelectedLocation({ ...searchedLocation });
+  // };
+  const doSearch = useDebounce((term) => {
+    const searchedLocation = getLocationByName(term.toLowerCase());
     console.log(searchedLocation);
     setSelectedLocation({ ...searchedLocation });
+  }, 1000);
+  const handleChange = (e) => {
+    const value = e.target.value;
+    doSearch(value);
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form>
       <div className="flex items-center space-x-2 py-2 px-3 group focus-within:bg-black/30 transition-all border-b border-white/50 focus-within:border-b-0 focus-within:rounded-md">
         <input
-          onChange={(e) => setSearchTerm(e.target.value)}
+          onChange={handleChange}
           className="bg-transparent  placeholder:text-white text-white w-full text-xs md:text-base outline-none border-none"
           type="search"
           placeholder="Search Location"
---
+++ src/components/hooks/useDebounce.js
@@ -0,0 +1,16 @@
+import { useRef } from "react"
+
+const useDebounce = (callback,delay)=>{
+    const timeOutIdRef = useRef(null)
+    const debouncedCallback=(...args)=>{
+        if(timeOutIdRef.current){
+            clearTimeout(timeOutIdRef.current)
+        }
+
+        timeOutIdRef.current = setTimeout(()=>{
+            callback(...args)
+        },delay)
+    }
+    return debouncedCallback
+}
+export default useDebounce;
